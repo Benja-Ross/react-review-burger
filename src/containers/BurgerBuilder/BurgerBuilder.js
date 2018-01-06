4d02803b6737b45e45d7e02b5d7e93c0b468782b
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-06 14:41:51 -0700

    build controls component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1, 
+            cheese: 1,
+            meat: 1
+        }
+    }
+
     render () {
         return (
             <Aux>
-                <Burger />
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls />
             </Aux>
         );
     }
