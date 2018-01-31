ac29f84f065ea13a387750d6b4d7b7f0296fe609
Benjamin Ross <benjaminross1001@gmail.com>
2018-01-31 09:00:28 -0700

    add logout functionality through redux and automatic logout after 1 hour

---
+++ src/store/actions/auth.js
@@ -0,0 +1,68 @@
+import axios from "axios";
+import keys from "../../config/keys";
+import * as actionTypes from "./actionTypes";
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = (idToken, userId) => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    idToken: idToken,
+    userId: userId
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
+
+export const logout = () => {
+  return {
+    type: actionTypes.AUTH_LOGOUT
+  };
+};
+
+export const checkAuthTimeout = expirationTime => {
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expirationTime * 1000);
+  };
+};
+
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    let url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${
+      keys.API_KEY
+    }`;
+    if (!isSignup) {
+      url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
+        keys.API_KEY
+      }`;
+    }
+    axios
+      .post(url, authData)
+      .then(response => {
+        console.log("RESPONSE: ", response);
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
+        dispatch(checkAuthTimeout(response.data.expiresIn));
+      })
+      .catch(error => {
+        console.log("ERROR: ", error);
+        dispatch(authFail(error.response.data.error));
+      });
+  };
+};
